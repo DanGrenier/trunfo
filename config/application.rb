2c4829d10340a0deb77cbfcb644c7da98ec1e0b4
DanGrenier <daniel.grenier@gmail.com>
2021-03-16 14:51:46 -0400

    Added basic error management

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,9 @@ module Trunfo
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :en 
+    config.exceptions_app = self.routes
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  get '/404', :to => "errors#not_found"
+  get '/422', :to => "errors#unacceptable"
+  get '/500', :to => "errors#server_error"
+
   root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
